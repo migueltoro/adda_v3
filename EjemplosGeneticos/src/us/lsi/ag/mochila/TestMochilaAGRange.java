c07c526ef9478b2c56dc6b4ab14cbaafe279638c
migueltoro <migueltoro@pc01>
2023-03-25 18:12:22 +0100

    25/03/2023

--- EjemplosGeneticos/src/us/lsi/ag/mochila/TestMochilaAGRange.java
+++ EjemplosGeneticos/src/us/lsi/ag/mochila/TestMochilaAGRange.java
@@ -14,17 +14,18 @@ import us.lsi.mochila.datos.SolucionMochila;
 public class TestMochilaAGRange {
 
 	public static void main(String[] args) {
-		Locale.setDefault(new Locale("en", "US"));
+		Locale.setDefault(Locale.of("en", "US"));
 		
 		AlgoritmoAG.ELITISM_RATE  = 0.30;
 		AlgoritmoAG.CROSSOVER_RATE = 0.8;
 		AlgoritmoAG.MUTATION_RATE = 0.7;
 		AlgoritmoAG.POPULATION_SIZE = 50;
 		
-		StoppingConditionFactory.NUM_GENERATIONS = 5000;
+		StoppingConditionFactory.NUM_GENERATIONS = 510000;
 		StoppingConditionFactory.SOLUTIONS_NUMBER_MIN = 1;
 		StoppingConditionFactory.FITNESS_MIN = 623;
-		StoppingConditionFactory.stoppingConditionType = StoppingConditionFactory.StoppingConditionType.SolutionsNumber;
+		StoppingConditionFactory.stoppingConditionType = 
+				StoppingConditionFactory.StoppingConditionType.SolutionsNumber;
 		
 		DatosMochila.capacidadInicial = 78;
 		DatosMochilaAGRange p = new DatosMochilaAGRange("ficheros/objetosmochila.txt");
--- Geneticos/src/us/lsi/ag/agstopping/SolutionsNumber.java
+++ Geneticos/src/us/lsi/ag/agstopping/SolutionsNumber.java
@@ -17,15 +17,15 @@ import us.lsi.common.List2;
  * 
  * <p> Acumula una lista, en la propiedad 
  * <code> AlgoritmoAG.bestChromosomes </code> que guarda los mejores <code> numBestChromosomes </code> 
- * distintos de cada generación.</p>
+ * distintos de cada generaciï¿½n.</p>
  * 
  * 
- * <p> Implementa una condición de parada que se cumple cuando se encuentran <code> numBestChromosomes </code> cromosomas
- * que cumplan un  <code> predicate </code> o el número de generaciones supera
+ * <p> Implementa una condiciï¿½n de parada que se cumple cuando se encuentran <code> numBestChromosomes </code> cromosomas
+ * que cumplan un  <code> predicate </code> o el nï¿½mero de generaciones supera
  * <code> AlgoritmoAG.NUM_GENERATIONS</code>  </p>
  * 
  * <p> La propiedad <code> predicate </code> debe ser inicializada desde el problema concreto a resolver. La propiedad numeroDeGeneraciones
- * guarda el número de generaciones </p>
+ * guarda el nï¿½mero de generaciones </p>
  * 
  * @author Miguel Toro
  *
@@ -56,7 +56,7 @@ public class SolutionsNumber implements StoppingCondition {
 		  .limit(ns)
 		  .collect(Collectors.toList());
 		AlgoritmoAG.bestChromosomes.addAll(ls);
-		boolean ret = fixed.isSatisfied(population);
+		Boolean ret = fixed.isSatisfied(population);
 		ret = ret || ls.stream().allMatch((Chromosome x) -> predicate.test(x));
 		numeroDeGeneraciones++;
 		return ret;
